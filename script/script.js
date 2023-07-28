a19738b94f5d0b7111ce24ad832f028485137ba9
Siddhesh <siddheshgovalkar1967@gmail.com>
2023-07-29 00:59:46 +0530

    ok

--- script/script.js
+++ script/script.js
@@ -1,27 +1,43 @@
 //  Readmore Functionality for Product Description on Home page
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const readMoreLinks = document.querySelectorAll('.read-more');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const readMoreLinks = document.querySelectorAll('.read-more');
 
-    readMoreLinks.forEach((link) => {
-      link.addEventListener('click', function (event) {
-        event.preventDefault();
-        const paragraph = this.previousElementSibling;
+//     readMoreLinks.forEach((link) => {
+//       link.addEventListener('click', function (event) {
+//         event.preventDefault();
+//         const paragraph = this.previousElementSibling;
 
-        if (paragraph.classList.contains('show-full-text')) {
-          // If paragraph is expanded, collapse it
-          paragraph.classList.remove('show-full-text');
-          this.textContent = 'Read More';
-        } else {
-          // If paragraph is collapsed, expand it
-          paragraph.classList.add('show-full-text');
-          this.textContent = 'Read Less';
-        }
-      });
+//         if (paragraph.classList.contains('show-full-text')) {
+//           // If paragraph is expanded, collapse it
+//           paragraph.classList.remove('show-full-text');
+//           this.textContent = 'Read More';
+//         } else {
+//           // If paragraph is collapsed, expand it
+//           paragraph.classList.add('show-full-text');
+//           this.textContent = 'Read Less';
+//         }
+//       });
+//     });
+//   });
+
+  $(document).ready(function () {
+    $('.read-more').click(function (event) {
+      event.preventDefault();
+      const paragraph = $(this).prev();
+
+      if (paragraph.hasClass('show-full-text')) {
+        // If paragraph is expanded, collapse it
+        paragraph.removeClass('show-full-text');
+        $(this).text('Read More');
+      } else {
+        // If paragraph is collapsed, expand it
+        paragraph.addClass('show-full-text');
+        $(this).text('Read Less');
+      }
     });
   });
 
-  
   
 
