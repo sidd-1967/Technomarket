ee240640f22f007782051a5feb94e86b49ba1b10
Siddhesh <siddheshgovalkar1967@gmail.com>
2023-07-30 01:53:15 +0530

    allproducts new changes & distorted aboutus page added.

--- script/script.js
+++ script/script.js
@@ -21,23 +21,25 @@
 //       });
 //     });
 //   });
-
-  $(document).ready(function () {
-    $('.read-more').click(function (event) {
-      event.preventDefault();
-      const paragraph = $(this).prev();
-
-      if (paragraph.hasClass('show-full-text')) {
-        // If paragraph is expanded, collapse it
-        paragraph.removeClass('show-full-text');
-        $(this).text('Read More');
-      } else {
-        // If paragraph is collapsed, expand it
-        paragraph.addClass('show-full-text');
-        $(this).text('Read Less');
-      }
-    });
+$(document).ready(function () {
+  $('.read-more').click(function (event) {
+    event.preventDefault();
+    
+    // Find the parent product-card of the clicked read-more button
+    const productCard = $(this).closest('.product-card');
+    
+    // Find the .truncate-text element within the specific product-card
+    const paragraph = productCard.find('.truncate-text');
+    
+    if (paragraph.hasClass('show-full-text')) {
+      // If paragraph is expanded, collapse it
+      paragraph.removeClass('show-full-text');
+      $(this).text('Read More');
+    } else {
+      // If paragraph is collapsed, expand it
+      paragraph.addClass('show-full-text');
+      $(this).text('Read Less');
+    }
   });
-
-  
+});
 
